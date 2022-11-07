83e111456bb9f66f116361ec6236cfabc7df3916
salvatoremallemaci <s303357@studenti.polito.it>
2022-11-07 11:54:17 +0100

    initial set up of project (React app + folder server)

---
+++ server/API.js
@@ -0,0 +1,8 @@
+const dao = require('./dao'); // module for accessing the DB
+const user_dao = require('./user-dao'); // module for accessing the users in the DB
+const { check, validationResult } = require('express-validator'); // validation middleware
+
+module.exports.useAPIs = function useAPIs(app, isLoggedIn) {
+
+
+}
---
+++ server/user-dao.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/* Data Access Object (DAO) module for accessing users */
+
+const sqlite = require('sqlite3');
+const crypto = require('crypto');
+
+// open the database
+const db = new sqlite.Database('hike_tracker.db', (err) => {
+	if (err) throw err;
+});
+
+// get the user identified by {id}
+exports.getUserById = (id) => {
+	return new Promise((resolve, reject) => {
+		const sql = `SELECT * FROM USER WHERE id=?`;
+
+		db.get(sql, [id], (err, row) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			if (row == undefined) {
+				resolve({ error: 'Utente non trovato.' });
+			} else {
+				const user = {
+					id: row.id,
+					username: row.username
+				};
+
+				resolve(user);
+			}
+		});
+	});
+};
+
+exports.getUser = (username, password) => {
+	return new Promise((resolve, reject) => {
+		const sql = 'SELECT * FROM USER WHERE username = ?';
+		db.get(sql, [username], (err, row) => {
+			if (err) { reject(err); }
+			else if (row === undefined) { resolve(false); }
+			else {
+				const user = { id: row.id, username: row.username, access_right: row.accessRight };
+
+				const salt = row.salt;
+				crypto.scrypt(password, salt, 32, (err, hashedPassword) => {
+					if (err) reject(err);
+
+					const passwordHex = Buffer.from(row.hash, 'hex');
+
+					if (!crypto.timingSafeEqual(passwordHex, hashedPassword))
+						resolve(false);
+					else resolve(user);
+				});
+			}
+		});
+	});
+};
