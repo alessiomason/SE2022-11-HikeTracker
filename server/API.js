12f7156a733f66c29513b620c6c7bc9697ebf5a9
violamudu <100765390+violamudu@users.noreply.github.com>
2022-11-08 16:09:31 +0100

    added upload of track front end

--- server/API.js
+++ server/API.js
@@ -5,4 +5,34 @@ const { check, validationResult } = require('express-validator'); // validation 
 module.exports.useAPIs = function useAPIs(app, isLoggedIn) {
 
 
+    // add new service type
+    app.post('/api/addGPXTrack', async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+console.log(req.body.features);
+        try {
+
+                let element=req.body.features[0];        
+                if (element.properties.name === '') {
+                    return res.status(422).json({ error: `Description name of track can not be empty.` });
+                }
+                trackName = element.properties.name;
+                pointsArray=element.geometry.coordinates;
+
+              //  const service = await dao.addGPXTrack(trackName);
+               // for (point in pointsArray){
+             //       await dao.addPoint(point[0],point);
+               // }
+             res.status(201).json(service).end();
+        } catch (err) {
+
+            res.status(500).json({ error: `Database error during creation of a new service.` });
+        }
+
+
+    });
+
 }
--- server/dao.js
+++ server/dao.js
@@ -8,3 +8,23 @@ const sqlite = require('sqlite3');
 const db = new sqlite.Database('hike_tracker.db', (err) => {
 	if (err) throw err;
 });
+exports.addGPXTrack = (trackName, pointsArray) => {
+	return new Promise((resolve, reject) => {
+		const sql = 'INSERT INTO CLIENT(name, idTicket, serviceType) VALUES(?, ?, ?)'
+		db.run(sql, [nameClient, idTicket, idService], function (err) {  
+		  if (err) {
+			reject(err);
+			return;
+		  }
+		  resolve(idTicket);
+		});
+
+
+
+
+
+
+
+		
+	  });
+}
