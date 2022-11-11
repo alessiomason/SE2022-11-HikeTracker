90754c3126c4e51d2a682eca087d9ece9fc64df7
violamudu <100765390+violamudu@users.noreply.github.com>
2022-11-11 10:34:11 +0100

    added apis for gpxTrack

--- server/API.js
+++ server/API.js
@@ -7,34 +7,34 @@ module.exports.useAPIs = function useAPIs(app, isLoggedIn) {
 
     // add new service type
     app.post('/api/addGPXTrack', async (req, res) => {
-
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         }
-       console.log(req.body.features[0].geometry.coordinates);
+        
         try {
 
-                let element=req.body.features[0];        
-               
-               
-               
-                if (element.properties.name === '') {
-                    return res.status(422).json({ error: `Description name of track can not be empty.` });
-                }
-                label = element.properties.name;
-                length = element.
-                pointsArray=element.geometry.coordinates;
+            let element = req.body.features[0];
+            if (element.properties.name === '') {
+                return res.status(422).json({ error: `Description name of track can not be empty.` });
+            }
+            label = element.properties.name;
+            pointsArray = element.geometry.coordinates;
+            ascent = pointsArray[pointsArray.length - 1][2] - pointsArray[0][2];
+            console.log(label)
+            console.log(ascent);
 
 
-              //  const service = await dao.addGPXTrack(trackName);
-               // for (point in pointsArray){
-             //       await dao.addPoint(point[0],point);
-               // }
-             res.status(201).json(service).end();
+            const service = await dao.addHike(label, null,null,ascent,null,"");
+            const hikeID = await dao.getLastHikeID();
+            console.log(hikeID);
+            for (point in pointsArray) {
+                await dao.addPoint(hikeID,point[1], point[0]); //lat and lon in the json representation are swapped
+            }
+            res.status(201).json(service).end();
         } catch (err) {
 
-            res.status(500).json({ error: `Database error during creation of a new service.` });
+            res.status(500).json({ error: `Database error during insertion of a new track.` });
         }
 
 
--- server/dao.js
+++ server/dao.js
@@ -8,36 +8,49 @@ const sqlite = require('sqlite3');
 const db = new sqlite.Database('hike_tracker.db', (err) => {
 	if (err) throw err;
 });
-exports.addPoint = (lat,lon,elevation) => {
+exports.addPoint = (hikeID,lat,lon) => {
 	return new Promise((resolve, reject) => {
-		const sql = 'INSERT INTO Points(Lat, Lon, Elevation) VALUES(?, ?, ?)'
-		db.run(sql, [lat, lon, elevation], function (err) {  
+		const sql = 'INSERT INTO Points(HikeID,Lat, Lon) VALUES(?, ?, ?)'
+		db.run(sql, [hikeID,lat, lon], function (err) {  
 		  if (err) {
 			reject(err);
 			return;
 		  }
-		  resolve(idTicket);
+		  resolve();
 		});		
 	  });
 }
 
 exports.addHike= (trackName, len,time,ascent,diff,description) => {
 	return new Promise((resolve, reject) => {
-		const sql = 'INSERT INTO Hikes(Label, Lenght, ExpTime,Ascent,Difficulty,Description) VALUES(?, ?, ?, ?, ?, ?)'
+		const sql = 'INSERT INTO Hikes(Label, Length, ExpTime,Ascent,Difficulty,Description) VALUES(?, ?, ?, ?, ?, ?)'
 		db.run(sql, [trackName, len,time,ascent,diff,description], function (err) {  
 		  if (err) {
 			reject(err);
 			return;
 		  }
-		  resolve(idTicket);
-		});
-
-
-
-
-
-
-
-		
+		  resolve();
+		});		
 	  });
+
+}
+
+exports.getLastHikeID = () => {
+	return new Promise((resolve, reject) => {
+		const sql = 'SELECT * FROM Hikes ORDER BY HikeID DESC LIMIT 1';
+		db.get(sql, [], (err, row) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			let hike;
+			if (row === undefined) {
+				 hike = { id: 0}
+			} else {
+				 hike = { id: row.HikeID}
+			}
+			
+			resolve(hike.id);
+		});
+	});
 }
