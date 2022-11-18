ddbaf491f5b1f29bde12d31a341e42799c371feb
luca-rota <lucarota981@gmail.com>
2022-11-19 00:10:46 +0100

    .

--- client/src/components/FilterSection.js
+++ client/src/components/FilterSection.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, ListGroup, Modal, Col, Row, Form, Container, ButtonToolbar, ButtonGroup } from "react-bootstrap";
 import '../styles/FilterSection.css';
+import { default as Close } from '../icons/close.svg';
 
 function FilterSection() {
 
@@ -14,7 +15,7 @@ function FilterSection() {
         <Row>
           <h1>Find your perfect destinations!</h1>
         </Row>
-        <Row className='mt-5 '>
+        <Row className='mt-5'>
           <Col md="auto" sm="auto" xs="auto" >
             <ButtonToolbar aria-label="Toolbar with button groups" >
               <ButtonGroup className='my-1' size="lg" aria-label="First group">
@@ -22,6 +23,7 @@ function FilterSection() {
                 <Button variant="success" onClick={() => { setModalShow(true); setTitle("Expected time"); setDesc("Select a specific expected time:") }}>Time</Button>
                 <Button variant="success" onClick={() => { setModalShow(true); setTitle("Ascent (meters)"); setDesc("Select a specific ascent:") }}>Ascent</Button>
                 <Button variant="success" className='btn_filter' onClick={() => { setModalShow(true); setTitle("Difficulty"); setDesc("Select a specific difficulty:") }}>Difficulty</Button>
+                <Button variant='success' className='btn_filter' onClick={() => { setModalShow(true); setTitle("Location"); setDesc("Select a specific location:") }}>Location</Button>
               </ButtonGroup>
               <ButtonGroup size="lg" className='my-1 me-2'>
                 <Button variant="success" className='btn_filter' onClick={() => { setModalShow(true); setTitle("Start point"); setDesc("Select a specific start point:") }}>Start point</Button>
@@ -31,10 +33,18 @@ function FilterSection() {
               <ButtonGroup className="my-1" aria-label="Second group">
                 <Button variant="danger">Remove filter</Button>
               </ButtonGroup>
-            </ButtonToolbar>
+              </ButtonToolbar>
           </Col>
         </Row>
         <MyModal show={modalShow} onHide={() => setModalShow(false)} title={title} desc={desc} />
+        <Row className='mt-3'>
+          <ButtonToolbar aria-label="Toolbar with button groups" >
+            <Button variant="info" disabled size="sm" className='mx-2 my-1 btn_info px-2'>Lenght: 10km <img src={Close} alt="close" className='ms-1 mb-1' /></Button>
+            <Button variant="info" disabled size="sm" className='mx-2 my-1 btn_info px-2'>Time: 1h <img src={Close} alt="close" className='ms-1 mb-1' /></Button>
+            <Button variant="info" disabled size="sm" className='mx-2 my-1 btn_info px-2'>Location: Torino <img src={Close} alt="close" className='ms-1 mb-1' /></Button>
+            <Button variant="info" disabled size="sm" className='mx-2 my-1 btn_info px-2'>Start point: Poli <img src={Close} alt="close" className='ms-1 mb-1' /></Button>
+          </ButtonToolbar>
+        </Row>
       </Container>
 
     );
@@ -82,7 +92,24 @@ function FilterSection() {
                   <ListGroup.Item action>Morbi leo risus</ListGroup.Item>
                   <ListGroup.Item action>Porta ac consectetur ac</ListGroup.Item>
                 </ListGroup> :
-                <Form.Range />}
+              (props.title == "Location") ? 
+                  <Container fluid> 
+                  <Form.Select className="my-3" aria-label="Select a province">
+                    <option>Select a province</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                  <Form.Select className="my-3" aria-label="Select a municipality">
+                    <option>Select a municipality</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                  <Button variant='success' className="btn_filter">Radius around a point</Button>
+                  </Container>
+                   :
+                  <Form.Range />}
           </Container>
         </Modal.Body>
   
