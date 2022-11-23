29d655f65899bd4253e40b1099f24f7b9c74c156
Eren Gul <eren.gul@ebrandvalue.com>
2022-11-23 15:23:23 +0100

    Docker files created

--- server/dao.js
+++ server/dao.js
@@ -5,7 +5,7 @@
 const sqlite = require('sqlite3');
 
 // open the database
-const db = new sqlite.Database('hike_tracker.db', (err) => {
+const db = new sqlite.Database('/db/hike_tracker.db', (err) => {
     if (err) throw err;
 });
 exports.addPoint = (hikeID, lat, lon,SP,EP,RP) => {
