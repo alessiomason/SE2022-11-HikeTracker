f63e07fddbdd60e161358bcbc5f551ebefe0cef2
Alessio Mason <alessio.mason@me.com>
2022-11-25 14:25:27 +0100

    Map to create parking lot

--- client/src/components/ParkingForm.js
+++ client/src/components/ParkingForm.js
@@ -1,19 +1,25 @@
 import { Alert, Form } from 'react-bootstrap';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { default as Img1 } from "../images/img1.jpg";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Icon } from 'leaflet';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import '../styles/Map.css';
+import 'leaflet/dist/leaflet.css';
 import '../styles/HikeForm.css';
 
 function ParkingForm(props) {
 
     const navigate = useNavigate();
 
-
     const [label, setLabel] = useState('');
     const [description, setDescription] = useState('');
     const [province, setProvince] = useState('');
     const [municipality, setMunicipality] = useState('');
+    const [latitude, setLatitude] = useState(0.0);
+    const [longitude, setLongitude] = useState(0.0);
+    const [altitude, setAltitude] = useState(0);
     const [errorMsg, setErrorMsg] = useState('');
 
     const handleSubmit = (event) => {
@@ -22,14 +28,14 @@ function ParkingForm(props) {
         if (label.trim().length === 0)
             setErrorMsg('The label of the hike cannot be consisted of only empty spaces');
         else {
-            const newParkingLot = { 
+            const newParkingLot = {
                 label: label,
                 description: description,
                 province: province,
                 municipality: municipality,
-                lat:2.0,
-                lon:2.0,
-                altitude:2.0
+                lat: latitude,
+                lon: longitude,
+                altitude: 2.0
             }
             props.addParkingLot(newParkingLot);
             props.setDirty(true);
@@ -71,11 +77,19 @@ function ParkingForm(props) {
                             </Form.Group>
 
                             <Form.Group>
+                                <Form.Label>Altitude [m]</Form.Label>
+                                <Form.Control required={true} type='number' step="any" value={altitude} onChange={ev => setAltitude(ev.target.value)} />
+                            </Form.Group>
+
+                            <Form.Group>
                                 <Form.Label>Description</Form.Label>
                                 <Form.Control required={true} value={description} onChange={ev => setDescription(ev.target.value)} />
                             </Form.Group>
                             <Row className='my-3 box_btn'>
-                                <Button variant="primary" className="btn_ref" >Add a reference point </Button>
+                                <div className='d-flex justify-content-center'>
+                                    <h3>Click on the map to select the parking's location</h3>
+                                </div>
+                                <ParkingMap setLatitude={setLatitude} setLongitude={setLongitude} />
                             </Row>
                             <Button className='save-button' type='submit' >Save</Button>
                             <Button className='back-button' onClick={() => navigate('/parkingManager')} variant='secondary' >Back</Button>
@@ -85,6 +99,41 @@ function ParkingForm(props) {
             </Row>
         </Container>
     );
+}
+
+function ParkingMap(props) {
+    return (
+        <MapContainer center={[45.177786, 7.083372]} zoom={10} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <LocationMarker setLatitude={props.setLatitude} setLongitude={props.setLongitude} />
+        </MapContainer>
+    );
+}
+
+function LocationMarker(props) {
+    const markerIcon = new Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    const [marker, setMarker] = useState([45.177786, 7.083372]);
+
+    useMapEvents({
+        click(e) {
+            props.setLatitude(e.latlng.lat);
+            props.setLongitude(e.latlng.lng);
+            setMarker([e.latlng.lat, e.latlng.lng]);
+        }
+    });
+
+    return (<Marker position={marker} icon={markerIcon} />);
 }
 
 export default ParkingForm;
