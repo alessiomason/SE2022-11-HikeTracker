6a3f36864cef0ffb834601b3a7dd2c655597ab70
Alessio Mason <alessio.mason@me.com>
2022-11-26 11:08:30 +0100

    Home carousel responsiveness fixed
    Controls and indicators removed from carousel

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -3,6 +3,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import { default as Image1 } from "../images/image1.jpg";
 import { default as Image2 } from "../images/image2.jpg";
 import { default as Image3 } from "../images/image3.jpg";
+import "../styles/Carousel.css";
 
 function MyCarousel() {
     const [index, setIndex] = useState(0);
@@ -13,15 +14,15 @@ function MyCarousel() {
 
     return (
 
-        <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel activeIndex={index} onSelect={handleSelect} controls={false} indicators={false}>
             <Carousel.Item>
-                <img className="d-block w-100" src={Image1} alt="First slide" />
+                <img className="d-block carousel-img" src={Image1} alt="First slide" />
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" src={Image2} alt="Second slide" />
+                <img className="d-block carousel-img" src={Image2} alt="Second slide" />
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" src={Image3} alt="Third slide" />
+                <img className="d-block carousel-img" src={Image3} alt="Third slide" />
             </Carousel.Item>
         </Carousel>
 
