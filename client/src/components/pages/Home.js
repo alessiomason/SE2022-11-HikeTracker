9a7dbb1747eb60b683c699d571bc71470e12ad81
Alessio Mason <alessio.mason@me.com>
2022-11-28 17:47:04 +0100

    Fixed filters; single filters can be removed
    Removed Province, Municipality and Points filters

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -9,18 +9,14 @@ import API from '../../API.js';
 
 function Home(props) {
 
-  const [minLength,setMinLength] = useState(0);
-  const [maxLength,setMaxLength] = useState(1000000);
-  const [minTime,setMinTime] = useState(0);
-  const [maxTime,setMaxTime] = useState(24);
-  const [minAscent,setMinAscent] = useState(0);
-  const [maxAscent,setMaxAscent] = useState(10000);
-  const [difficulty,setDifficulty] = useState(null);
-  const [municipality,setMunicipality] = useState(null);
-  const [province,setProvince] = useState(null);
-  const [startPoint,setStartPoint] = useState (null);
-  const [endPoint,setEndPoint] = useState (null);
-  const [refPoint,setRefPoint] = useState (null);
+  // filters
+  const [minLength,setMinLength] = useState('');
+  const [maxLength,setMaxLength] = useState('');
+  const [minTime,setMinTime] = useState('');
+  const [maxTime,setMaxTime] = useState('');
+  const [minAscent,setMinAscent] = useState('');
+  const [maxAscent,setMaxAscent] = useState('');
+  const [difficulty,setDifficulty] = useState('');
 
   const [hikes, setHikes] = useState([]);
   const [dirty, setDirty] = useState(true);
@@ -37,14 +33,12 @@ function Home(props) {
 
   return (
     <>
-    
       <MyTopHome setShowEmailAlert={props.setShowEmailAlert} showEmailAlert={props.showEmailAlert}/>
       <MyModalLogin setShowLogin={props.setShowLogin} showLogin={props.showLogin} user={props.user} loggedIn={props.loggedIn} doLogin={props.doLogin} message={props.message} setMessage={props.setMessage} />
       <MyModalSignup setShowEmailAlert={props.setShowEmailAlert} setShowSignup={props.setShowSignup} showSignup={props.showSignup} setMessage={props.setMessage} doSignUp={props.doSignUp} message={props.message} />
       <MyFilterSection  hikes={hikes} minLength={minLength} setMinLength={setMinLength} maxLength={maxLength} setMaxLength={setMaxLength} minTime={minTime} setMinTime={setMinTime} maxTime={maxTime} setMaxTime={setMaxTime}
-                        minAscent={minAscent} setMinAscent={setMinAscent} maxAscent={maxAscent} setMaxAscent={setMaxAscent} difficulty={difficulty} setDifficulty={setDifficulty} 
-                        municipality={municipality}  setMunicipality={setMunicipality} province={province} setProvince={setProvince} startPoint={startPoint} setStartPoint={setStartPoint} refPoint={refPoint} setRefPoint={setRefPoint} endPoint={endPoint} setEndPoint={setEndPoint}/>
-      <HikesCards user={props.user} setMinLength={setMinLength} minLength={minLength} maxLength={maxLength} minTime={minTime} maxTime={maxTime} minAscent={minAscent} maxAscent={maxAscent} difficulty={difficulty}  municipality={municipality} province={province} startPoint={startPoint} endPoint={endPoint} refPoint={refPoint}
+                        minAscent={minAscent} setMinAscent={setMinAscent} maxAscent={maxAscent} setMaxAscent={setMaxAscent} difficulty={difficulty} setDifficulty={setDifficulty} />
+      <HikesCards user={props.user} minLength={minLength} maxLength={maxLength} minTime={minTime} maxTime={maxTime} minAscent={minAscent} maxAscent={maxAscent} difficulty={difficulty}
                   hikes={hikes}   />
     </>
   );
