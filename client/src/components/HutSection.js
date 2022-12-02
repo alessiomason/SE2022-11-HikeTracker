a82e6843a31312ce954ffaf9126fdadd90bc4521
luca-rota <lucarota981@gmail.com>
2022-12-02 18:31:17 +0100

    .

--- client/src/components/HutSection.js
+++ client/src/components/HutSection.js
@@ -1,8 +1,9 @@
-import { Container, Row, Col, Button, ButtonGroup, ButtonToolbar  } from "react-bootstrap";
+import { Container, Row, Col, Button, ButtonGroup, ButtonToolbar, OverlayTrigger, Tooltip,  Overlay  } from "react-bootstrap";
 import '../styles/HutSection.css';
 import { default as Hut } from '../icons/hut.svg';
 import { default as arrowRight } from '../icons/arrow-right.svg';
 import { default as arrowLeft } from '../icons/arrow-left.svg';
+import { default as Delete } from '../icons/delete.svg';
 import { FaSearch } from "react-icons/fa";
 import { FaBed } from "react-icons/fa";
 import { FaLocationArrow } from "react-icons/fa";
@@ -10,7 +11,7 @@ import { FaMountain } from "react-icons/fa";
 import { ImPriceTag } from "react-icons/im";
 
 
-import React, { useState } from 'react';
+import React, { useState, useRef  } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -18,6 +19,12 @@ import 'slick-carousel/slick/slick-theme.css';
 import { default as image4 } from "../images/image4.jpg";
 
 function MyHutSection() {
+
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Remove all filters
+    </Tooltip>
+  );
 
   return (
     <>
@@ -34,18 +41,21 @@ function MyHutSection() {
       <Row className='mt-5'>
         <Col md="auto" sm="auto" xs="auto" >
           <ButtonToolbar aria-label="Toolbar with button groups" >
-            <ButtonGroup className='my-1  me-2' size="lg" aria-label="First group">
-              <Button variant="success" className='btn_filter btn-filter-hut'>location</Button>
+            <ButtonGroup className='my-1  me-3' size="lg" aria-label="First group">
+              <Button variant="success" className='btn_filter btn-filter-hut'>Location</Button>
               <Button variant="success" className='btn_filter btn-filter-hut'>Ascent</Button>
               <Button variant="success" className='btn_filter btn-filter-hut' >Number of beds</Button>
               <Button variant='success' className='btn_filter btn-filter-hut' >Services</Button>
-            </ButtonGroup>
-            <ButtonGroup className="my-1" aria-label="Second group">
-              <Button variant="danger" className="remove-filter" >Remove all filters</Button>
-            </ButtonGroup>
-          </ButtonToolbar>
+              </ButtonGroup>
+              <ButtonGroup className="my-1" aria-label="Second group">
+                <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderTooltip} >
+                  <Button className="delete-btn"><img src={Delete} alt="delete_image" className='' /></Button>
+                </OverlayTrigger>
+              </ButtonGroup>
+            </ButtonToolbar>
         </Col>
       </Row>
+      
     </Container>
     <Container fluid className="hutCardSection">
       <Row>
@@ -84,7 +94,7 @@ function Card() {
     prevArrow: <SamplePrevArrow />,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1120,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
@@ -93,7 +103,7 @@ function Card() {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 720,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
@@ -110,33 +120,42 @@ function Card() {
     ],
   };
 
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Simple tooltip
+    </Tooltip>
+  );
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
   return (
     <div className="hutCardBox">
       <Slider {...settings}>
         {huts.map((item) => (
           <div className="hut-card">
             <div className="card-top">
-              <img src={ image4 } alt={item.title} />
-              <h1>{item.title}</h1>
+              <img src={image4} alt={item.title} className="card-top-img" />
             </div>
             <div className="card-bottom">
-              <Row>
-                <Col md={6}>
-                <FaLocationArrow className="card-symbol mb-4 me-3"/> <h6 className="card-details">{item.location}</h6>
+              <div>
+                <h1 className="hut-card-title">{item.title}</h1>
+              </div>
+              <Row >
+                <Col md={12} lg={12} xl={12} xxl={6} className="mb-1">
+                  <FaLocationArrow className="card-symbol me-3" /><h6 className="card-details">{item.location}</h6>
                 </Col>
-                <Col md={6}>
-                <FaMountain className="card-symbol mb-4 me-3"/> <h6 className="card-details">{item.ascent}</h6>
-                </Col>
-              </Row>
-              <Row>
-                <Col md={6}>
-                <ImPriceTag  className="card-symbol mb-4 me-3"/> <h6 className="card-details">{item.price}</h6>
-                </Col>
-                <Col md={6}>
-                <FaBed className="card-symbol mb-4 me-3"/> <h6 className="card-details">{item.beds}</h6>
+                <Col md={12} lg={12} xl={12} xxl={6} className="mb-1">
+                  <FaMountain className="card-symbol me-3" /> <h6 className="card-details">{item.ascent}</h6>
                 </Col>
               </Row>
-            
+              <Row>
+                <Col md={12} lg={6} sm={12} className="mb-1" >
+                  <ImPriceTag className="card-symbol me-3" /> <h6 className="card-details">{item.price}</h6>
+                </Col>
+                <Col md={12} lg={6} sm={12} className="mb-1">
+                  <FaBed className="card-symbol me-3" /> <h6 className="card-details">{item.beds}</h6>
+                </Col>
+              </Row>
             </div>
           </div>
         ))}
@@ -149,65 +168,65 @@ const huts = [
   {
     id: 1,
     title: 'Cascina bianca',
-    location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    location: 'Garessio, Cuneo',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
   },
   {
     id: 2,
     title: 'Cascina nera',
     location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
     },
   {
     id: 3,
     title: 'Cascina corta',
     location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
    },
   {
     id: 4,
     title: 'Cascina lunga',
     location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
    },
   {
     id: 5,
     title: 'Cascina bella',
     location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
     },
   {
     id: 6,
     title: 'Cascina brutta',
     location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
     },
   {
     id: 7,
     title: 'Cascina alta',
     location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
      },
   {
     id: 8,
     title: 'Cascina bassa',
     location: 'politecnico',
-    ascent: '1200 m',
-    price: '200 $',
+    ascent: '1200m',
+    price: '200$',
     beds: '150',
    },
 ];
