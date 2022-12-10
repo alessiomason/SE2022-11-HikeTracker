290d0e7321cdf91e3843c1de754a933e6d2cfe06
Alessio Mason <alessio.mason@me.com>
2022-12-10 11:58:40 +0100

    Working Show more button

--- client/src/components/HikeCards.js
+++ client/src/components/HikeCards.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Row, Col, Container, Button } from "react-bootstrap";
 import SingleHikeCard from './SingleHikeCard';
 import { default as Arrow } from "../icons/arrow-down.svg";
@@ -18,27 +19,29 @@ function coordinatesDistanceInMeter(lat1, lon1, lat2, lon2) {  // generally used
 
 function HikesCards(props) {
 
+    const [showingAll, setShowingAll] = useState(false);    // state for Show more button
+
     return (
         <Container fluid className="cards p-5">
             <Row className='mb-3 box_centered'>
                 {props.hikes.filter(h => {
                     if (props.hikesMinLength && h.length < props.hikesMinLength
-                    || props.hikesMaxLength && h.length > props.hikesMaxLength
-                    || props.hikesMinTime && h.expTime < props.hikesMinTime
-                    || props.hikesMaxTime && h.expTime > props.hikesMaxTime
-                    || props.hikesMinAscent && h.ascent < props.hikesMinAscent
-                    || props.hikesMaxAscent && h.ascent > props.hikesMaxAscent
-                    || !props.hikesDifficulties.find(d => d.level === parseInt(h.difficulty)).isChecked
-                    || props.hikesState && h.state !== props.hikesState
-                    || props.hikesRegion && h.region !== props.hikesRegion
-                    || props.hikesProvince && h.province !== props.hikesProvince
-                    || props.hikesMunicipality && h.municipality !== props.hikesMunicipality
-                    || props.hikesLatitude !== -1 && props.hikesLongitude !== -1 && props.hikesRadius !== -1 &&
-                       coordinatesDistanceInMeter(h.startPoint.latitude, h.startPoint.longitude, props.hikesLatitude, props.hikesLongitude) > props.hikesRadius * 1000)
+                        || props.hikesMaxLength && h.length > props.hikesMaxLength
+                        || props.hikesMinTime && h.expTime < props.hikesMinTime
+                        || props.hikesMaxTime && h.expTime > props.hikesMaxTime
+                        || props.hikesMinAscent && h.ascent < props.hikesMinAscent
+                        || props.hikesMaxAscent && h.ascent > props.hikesMaxAscent
+                        || !props.hikesDifficulties.find(d => d.level === parseInt(h.difficulty)).isChecked
+                        || props.hikesState && h.state !== props.hikesState
+                        || props.hikesRegion && h.region !== props.hikesRegion
+                        || props.hikesProvince && h.province !== props.hikesProvince
+                        || props.hikesMunicipality && h.municipality !== props.hikesMunicipality
+                        || props.hikesLatitude !== -1 && props.hikesLongitude !== -1 && props.hikesRadius !== -1 &&
+                        coordinatesDistanceInMeter(h.startPoint.latitude, h.startPoint.longitude, props.hikesLatitude, props.hikesLongitude) > props.hikesRadius * 1000)
                         return false;
 
                     return true;
-                }).map(h => {
+                }).slice(0, showingAll ? undefined : 9).map(h => {
                     return (
                         <Col xl={4} lg="auto" md="auto" sm="auto" xs="auto" key={h.id} >
                             <SingleHikeCard fromHikeCards key={h.id} hike={h} user={props.user} />
@@ -47,9 +50,9 @@ function HikesCards(props) {
                 })}
             </Row>
             <Row className="box_centered">
-                <Button variant="primary" className="btn_show my-3" >
+                {!showingAll && <Button variant="primary" className="btn_show my-3" onClick={() => setShowingAll(true)}>
                     Show more <img className="ms-2 " src={Arrow} alt="arrow_image" />
-                </Button>
+                </Button>}
                 {/*<div id="container">
                     <button class=" learn-more">
                         <span class="circle" aria-hidden="true">
