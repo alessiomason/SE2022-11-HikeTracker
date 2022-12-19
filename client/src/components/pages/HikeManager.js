c5cf972738b1aaade550248ab8e562fa8fd017fd
salvatoremallemaci <s303357@studenti.polito.it>
2022-12-19 12:25:44 +0100

    Now are allowed only gpx files for tracks, jpg files for images, 10 MB limit

--- client/src/components/pages/HikeManager.js
+++ client/src/components/pages/HikeManager.js
@@ -123,7 +123,7 @@ function SingleUpdateHikeCard(props) {
         />
         <Form.Group controlId="formFile" className="mb-3">
           <Form.Label className='updateImageButton'>Update Image</Form.Label>
-          <Form.Control type="file"
+          <Form.Control type="file" accept='.jpg'
             onChange={(e) => { setImage(e.target.files[0]); setPreview(URL.createObjectURL(e.target.files[0])) }} />
         </Form.Group>
       </Col>
--- client/src/components/pages/HutManager.js
+++ client/src/components/pages/HutManager.js
@@ -120,7 +120,7 @@ function SingleUpdateHutCard(props) {
            />
           <Form.Group controlId="formFile" className="mb-3">
             <Form.Label className='updateImageHut'>Update Image</Form.Label>
-            <Form.Control type="file"
+            <Form.Control type="file" accept='.jpg'
               onChange={(e) => { setImage(e.target.files[0]); setPreview(URL.createObjectURL(e.target.files[0]))} }/>
           </Form.Group>
         </Col>
