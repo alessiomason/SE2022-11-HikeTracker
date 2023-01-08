f9d56502d5509e09193f633562cc05b0ba7a04d3
Alessio Mason <alessio.mason@me.com>
2023-01-08 15:13:39 +0100

    Fixed tracked hikes unit tests

--- server/unit_test/trackedHikes.test.js
+++ server/unit_test/trackedHikes.test.js
@@ -3,7 +3,7 @@ const dayjs = require('dayjs');
 const user_dao = require('../user-dao'); // module for accessing the DB
 
 describe("test tracked hikes functions", () => {
-    deleteAllTrackedHikesTest();
+	deleteAllTrackedHikesTest();
 	createUser();
 	startHikeTest();
 	deleteAllTrackedHikesTest();
@@ -12,67 +12,93 @@ describe("test tracked hikes functions", () => {
 })
 
 function deleteAllTrackedHikesTest() {
-    test('delete all tracked hikes', async () => {
-        await dao.deleteAllTrackedHikes();
+	test('delete all tracked hikes', async () => {
+		await dao.deleteAllTrackedHikes();
 
 		let res = await dao.getTrackedHikesByHikeIDAndUserID(1, 3);
-        expect(res.length).toStrictEqual(0);
+		expect(res.length).toStrictEqual(0);
 
 		res = await dao.getTrackedHikesByUserID(3);
-        expect(res.length).toStrictEqual(0);
-    });
+		expect(res.length).toStrictEqual(0);
+	});
 }
 
 function startHikeTest() {
-    test('start a new hike', async () => {
+	test('start a new hike', async () => {
+		let data = {
+			label: "Gran Paradiso",
+			length: 20500,
+			expTime: 10,
+			ascent: 23,
+			difficulty: "3",
+			description: "Alpine challenge",
+			state: "Italy",
+			region: "Piedmont",
+			province: "Torino",
+			municipality: "Cogne",
+			authorID: 1
+		};
+
+		await dao.newHike(data.label,
+			data.length,
+			data.expTime,
+			data.ascent,
+			data.difficulty,
+			data.description,
+			data.state,
+			data.region,
+			data.province,
+			data.municipality,
+			data.authorID);
+
 		const startTime = dayjs().format();
 
 		await dao.startHike(1, 3, "1/3", startTime);
 
-        let res = await dao.getTrackedHikesByHikeIDAndUserID(1, 3);
-        expect(res.length).toStrictEqual(1);
+		let res = await dao.getTrackedHikesByHikeIDAndUserID(1, 3);
+		expect(res.length).toStrictEqual(1);
 		expect(res[0].hikeID).toStrictEqual(1);
 		expect(res[0].startTime).toStrictEqual(startTime);
 
 		res = await dao.getTrackedHikesByUserID(3);
-        expect(res.length).toStrictEqual(1);
+		expect(res.length).toStrictEqual(1);
 		expect(res[0].hikeID).toStrictEqual(1);
 		expect(1).toStrictEqual(1);
-    });
+	});
 }
 
 
 function terminateHikeTest() {
-    test('terminate an hike', async () => {
+	test('terminate an hike', async () => {
 		const startTime = dayjs().format();
 		const endTime = dayjs().format();
 
-		await dao.startHike(1, 3,"0/0", startTime);
-        await dao.terminateHike(1, endTime, "0/0");
-		
+		await dao.startHike(1, 3, "0/0", startTime);
+		await dao.terminateHike(1, endTime, "0/0");
+
 		let res = await dao.getTrackedHikesByHikeIDAndUserID(1, 3);
-        expect(res.length).toStrictEqual(1);
+		expect(res.length).toStrictEqual(1);
 		expect(res[0].hikeID).toStrictEqual(1);
 		expect(res[0].startTime).toStrictEqual(startTime);
 		expect(res[0].endTime).toStrictEqual(endTime);
 
 		res = await dao.getTrackedHikesByUserID(3);
-        expect(res.length).toStrictEqual(1);
+		expect(res.length).toStrictEqual(1);
 		expect(res[0].hikeID).toStrictEqual(1);
 		expect(res[0].startTime).toStrictEqual(startTime);
 		expect(res[0].endTime).toStrictEqual(endTime);
-    });
+	});
 }
 
 function createUser() {
-	test('create new user', async () => {  
+	test('create new user', async () => {
 		const data = {
-            email: 'group11@p.it',
-            password: 'ciao',
-            accessRight: 'hiker'
-        };
-  
+			email: 'group11@p.it',
+			password: 'ciao',
+			accessRight: 'hiker'
+		};
+
 		await user_dao.newUser(data.email, data.password, data.accessRight);
-  
+
 	});
-  }
+}
